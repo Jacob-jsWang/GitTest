3b01ada8475153d6c83f681c0f2b16ad8a382e38
WangJS <1135340412@qq.com>
2021-02-19 18:47:00 +0800

    second commit

--- src/main/java/com/atguigu/hello.java
+++ src/main/java/com/atguigu/hello.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,hot-fix");
+        System.out.println("1212121212");
     }
 }
